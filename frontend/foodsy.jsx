e0bc1886bee8b53205cc9d9d394d734ec3ebbd54
App Academy Student <student@appacademy.io>
2016-06-28 11:54:35 -0700

    created static_page

---
+++ frontend/foodsy.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+document.addEventListener('DOMContentLoaded', function() {
+  const root = document.getElementById('content');
+  ReactDOM.render(root);
+});
