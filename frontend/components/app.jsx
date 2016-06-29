317eea912dd891a07eab7426c3aa2748bff728c3
App Academy Student <student@appacademy.io>
2016-06-29 12:12:44 -0700

    update production.rb for assets on heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 const Link = require('react-router').Link;
 const SessionStore = require('../stores/session_store');
 const SessionActions = require('../actions/session_actions');
+const Modal = require('react-modal');
 
 const App = React.createClass({
   contextTypes: {
