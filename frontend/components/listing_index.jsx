cc81d1616f47ba65748b7388abdd1b3183816623
jonathol <jonathonlin93@yahoo.com>
2017-02-15 21:13:38 +0000

    fix listing items alignment

--- frontend/components/listing_index.jsx
+++ frontend/components/listing_index.jsx
@@ -34,10 +34,10 @@ const ListingIndex = React.createClass({
                 />;
             })
           }
-          <div class="filler-div"></div>
-          <div class="filler-div"></div>
-          <div class="filler-div"></div>
-          <div class="filler-div"></div>
+          <div class="filler-div">&nbsp;</div>
+          <div class="filler-div">&nbsp;</div>
+          <div class="filler-div">&nbsp;</div>
+          <div class="filler-div">&nbsp;</div>
         </div>
       </div>
     );
